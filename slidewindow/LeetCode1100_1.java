5740de08273a2aaaf74f4622193565c0c77e9f4a
smzj-star <67086461+smzj-star@users.noreply.github.com>
2022-07-24 19:50:21 +0800

    Create LeetCode1100_1.java
    时间复杂度高；T(n)=O(n*k),其中n为字符串长度,k为不重复子串长度

---
+++ slidewindow/LeetCode1100_1.java
@@ -0,0 +1,56 @@
+/**
+给你一个字符串 S，找出所有长度为 K 且不含重复字符的子串，请你返回全部满足要求的子串的 数目。
+
+ 
+
+示例 1：
+
+输入：S = "havefunonleetcode", K = 5
+输出：6
+解释：
+这里有 6 个满足题意的子串，分别是：'havef','avefu','vefun','efuno','etcod','tcode'。
+示例 2：
+
+输入：S = "home", K = 5
+输出：0
+解释：
+注意：K 可能会大于 S 的长度。在这种情况下，就无法找到任何长度为 K 的子串。
+ 
+
+提示：
+
+1 <= S.length <= 10^4
+S 中的所有字符均为小写英文字母
+1 <= K <= 10^4
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/find-k-length-substrings-with-no-repeated-characters
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+class Solution {
+    public int numKLenSubstrNoRepeats(String s, int k) {
+        char[] array = s.toCharArray();
+        int len = array.length;
+        if (len < k) {
+            return 0;
+        }
+        int res = 0;
+        int left = 0;
+        int[] temp;
+        while (left < len) {
+            temp = new int[128];
+            int count = 0;
+            for (int right = left; right < left + k && right < len; right++) {
+                if (temp[array[right]] == 0) {
+                    count++;
+                }
+                temp[array[right]]++;
+            }
+            if (count == k) {
+                res++;
+            }
+            left++;
+        }
+        return res;
+    }
+}
