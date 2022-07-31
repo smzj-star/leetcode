5c9839e8d6978c1c5371546ebf07311163ac6582
smzj-star <67086461+smzj-star@users.noreply.github.com>
2022-07-31 12:07:47 +0800

    Create LeetCode102.java

---
+++ bfs/LeetCode102.java
@@ -0,0 +1,75 @@
+/**
+给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+
+ 
+
+示例 1：
+
+
+输入：root = [3,9,20,null,null,15,7]
+输出：[[3],[9,20],[15,7]]
+示例 2：
+
+输入：root = [1]
+输出：[[1]]
+示例 3：
+
+输入：root = []
+输出：[]
+ 
+
+提示：
+
+树中节点数目在范围 [0, 2000] 内
+-1000 <= Node.val <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/binary-tree-level-order-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+执行用时：1 ms, 在所有 Java 提交中击败了57.14%的用户
+内存消耗：41.7 MB, 在所有 Java 提交中击败了20.17%的用户
+*/
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.add(root);
+        List<Integer> level;
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = deque.poll();
+                level.add(cur.val);
+                if (cur.left != null) {
+                    deque.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    deque.offer(cur.right);
+                }
+            }
+            list.add(level);
+        }
+        return list;
+    }
+}
