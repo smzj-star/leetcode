5f20d88317901b5beeb111c1e978977a491e559b
smzj-star <67086461+smzj-star@users.noreply.github.com>
2022-09-07 20:23:53 +0800

    Create LeetCode828.java

---
+++ dp/LeetCode828.java
@@ -0,0 +1,64 @@
+/**
+我们定义了一个函数 countUniqueChars(s) 来统计字符串 s 中的唯一字符，并返回唯一字符的个数。
+
+例如：s = "LEETCODE" ，则其中 "L", "T","C","O","D" 都是唯一字符，因为它们只出现一次，所以 countUniqueChars(s) = 5 。
+
+本题将会给你一个字符串 s ，我们需要返回 countUniqueChars(t) 的总和，其中 t 是 s 的子字符串。输入用例保证返回值为 32 位整数。
+
+注意，某些子字符串可能是重复的，但你统计时也必须算上这些重复的子字符串（也就是说，你必须统计 s 的所有子字符串中的唯一字符）。
+
+ 
+
+示例 1：
+
+输入: s = "ABC"
+输出: 10
+解释: 所有可能的子串为："A","B","C","AB","BC" 和 "ABC"。
+     其中，每一个子串都由独特字符构成。
+     所以其长度总和为：1 + 1 + 1 + 2 + 2 + 3 = 10
+示例 2：
+
+输入: s = "ABA"
+输出: 8
+解释: 除了 countUniqueChars("ABA") = 1 之外，其余与示例 1 相同。
+示例 3：
+
+输入：s = "LEETCODE"
+输出：92
+ 
+
+提示：
+
+1 <= s.length <= 10^5
+s 只包含大写英文字符
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/count-unique-characters-of-all-substrings-of-a-given-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+class Solution {
+    public int uniqueLetterString(String s) {
+        // dp[i][0]表示字符(i + 'A')造成的正面影响
+        // dp[i][1]表示字符(i + 'A')出现的索引
+        int[][] dp = new int[26][2];
+        for (int i = 0; i < 26; i++) {
+            // 初始化表示A-Z从未出现过
+            dp[i][1] = -1;
+        }
+        char[] charArray = s.toCharArray();
+        int length = charArray.length;
+        int res = 0;
+        // 表示新字符添加进来以后可以多多少个值
+        int preSum = 0;
+        for (int i = 0; i < length; i++) {
+            int index = charArray[i] - 'A';
+            // 表示字符(i + 'A')造成的正面影响
+            int currentCount = i - dp[index][1];
+            preSum = preSum + currentCount - dp[index][0];
+            dp[index][0] = currentCount;
+            dp[index][1] = i;
+            res += preSum;
+        }
+        return res;
+    }
+}
