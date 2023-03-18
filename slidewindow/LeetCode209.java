038ba6e94d7d42b980525daec88b1a493d94a19f
smzj-star <67086461+smzj-star@users.noreply.github.com>
2023-03-18 11:50:59 +0800

    Create LeetCode209.java

---
+++ slidewindow/LeetCode209.java
@@ -0,0 +1,54 @@
+/**
+给定一个含有 n 个正整数的数组和一个正整数 target 。
+
+找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+
+ 
+
+示例 1：
+
+输入：target = 7, nums = [2,3,1,2,4,3]
+输出：2
+解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+示例 2：
+
+输入：target = 4, nums = [1,4,4]
+输出：1
+示例 3：
+
+输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+输出：0
+ 
+
+提示：
+
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/minimum-size-subarray-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int len = nums.length;
+        int sum = 0;
+        int left = 0;
+        int right = 0;
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] >= target) {
+                return 1;
+            }
+            sum += nums[right];
+            while (sum >= target && left <= right) {
+                result = Math.min(result, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+}
