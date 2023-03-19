cde530d9827b36b288b53ac6f8913d47c746bc15
smzj-star <67086461+smzj-star@users.noreply.github.com>
2023-03-19 15:10:16 +0800

    Create LeetCode213_1.java

---
+++ dp/LeetCode213_1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        }
+        if (len == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[len];
+        return Math.max(robRange(nums, 0, len -2, dp), robRange(nums, 1, len - 1, dp));
+    }
+
+    private int robRange(int[] nums, int start, int end, int[] dp) {
+        dp[start] = nums[start];
+        dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[end];
+    }
+}
