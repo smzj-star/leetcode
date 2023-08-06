4aeeca0b83541e0fd37943f2b8b925375f7067b8
smzj-star <67086461+smzj-star@users.noreply.github.com>
2023-08-06 10:46:35 +0800

    Create LeetCode337.java

---
+++ dfs/LeetCode337.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] result = dfs(root);
+        return Math.max(result[0], result[1]);
+    }
+
+    private int[] dfs(TreeNode root) {
+        if (root == null) {
+            return new int[2];
+        }
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+        int withRoot = root.val + left[1] + right[1];
+        int withoutRoot = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return new int[]{withRoot, withoutRoot};
+    }
+}
